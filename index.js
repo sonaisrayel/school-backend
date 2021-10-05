6d8101a76abbaaf20f04bdf935bde5a62d960029
Robert-Astanyan <robast43@gmail.com>
2021-10-06 01:42:23 +0400

    'API-3'Rob

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express();
 const fs = require('fs');
 const student = require("./student.json")
-const port = 4000
+const personal = require("./student-personal.json")
+const profesion = require("./profesion.json")
 
-app.get('/student-personal', (req, res) => {
-//    fs.writeFileSync('req.json', JSON.stringify(req.query))
+const port = 3000
+
+app.get('/student', (req, res) => {
+   //    fs.writeFileSync('req.json', JSON.stringify(req.query))
    res.send(student)
 })
 
+app.get('/student-personal', (req, res) => {
+   //    fs.writeFileSync('req.json', JSON.stringify(req.query))
+   res.send(personal)
+})
+
+app.get('/profesion', (req, res) => {
+   //    fs.writeFileSync('req.json', JSON.stringify(req.query))
+   res.send(profesion)
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+   console.log(`Example app listening at http://localhost:${port}`)
 })

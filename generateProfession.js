220ce88a02e489b5988c2667f7e83496d7d11075
Sona Israyelyan <sonai@brightcom.com>
2021-10-08 12:08:59 +0400

    Add new changes

--- generateProfession.js
+++ generateProfession.js
@@ -1,4 +1,26 @@
-const profession = [];
-const faculty = [];
-const pay = [];
-const study_year = []
+const fs = require('fs');
+
+
+const profession = ["History", "Education Management", "Pharmacy", "Church Studies", "Public Management", "English Language and Literature", "Sociology", "Radiophysics and Electronics", "Informatics and Applied Mathematics", "Psychology", "Journalism", "Human resource management", "Economics", "Public Finance", "Marketing Communications", "Auditing", "Applied Statistics and data science", "Pedagogy and Methodology (elementary education)", "Logopaedics", "Arabic Studies", "Russian Language, Linguistics and Cross-cultural Communication", "Jurisprudence", "Geology", "Biology", "Mechanics", "Economics", "Physics, Nuclear Reactor Physics", "Environmental Chemistry", "Armenian Language and Literature", "Art Studies", "Physical culture and sport", "Kinesiologist"];
+const faculty = ["Faculty of History", "Faculty of Armenian Philology", "Faculty of Chemistry", "Faculty of Physics", "Faculty of Economics and Management", "Faculty of Mathematics and Mechanics", "Faculty of Biology", "Faculty of Geography and Geology", "Faculty of Law", "Faculty of Russian Philology", "Faculty of Oriental Studies", "Faculty of Journalism", "Faculty of Philosophy and Psychology", "Faculty of Informatics and Applied Mathematics", "Faculty of Radiophysics", "Faculty of Sociology", "Faculty of European Languages and Communication", "Faculty of International Relations", "Faculty of Theology", "Institute of Pharmacy", "Pedagogy and Education Development Center", "Faculty of Health Technologies, Sports Tourism and Management", "Coaching and pedagogical faculty", "Faculty of Management", "Faculty of Finance", "Faculty of Special and Inclusive Education", "Faculty of Marketing and Business Management", "Faculty of Computer Science and Statistics", "Faculty of Accounting and Auditing", "Faculty of Primary Education"];
+const pay = ["1000000", "300000", "480000", "250000", "560000", "600000", "410000", "550000", "1200000", "200000", "841000", "235470", "400000", "600000", "845100"];
+const study_year = ["2017-2018", "2018-2019", "2011-2012", "2019-2020", "2004-2005", "2020-2021", "2005-2006", "2001-2002", "2002-2003", "2008-2009", "2015-2016", "2016-2017"]
+
+let array = [];
+
+
+for (let i = 0; i < profession.length; i++) {
+
+    if (study_year[i] && profession[i] && pay[i] && faculty[i]) {
+        array.push({
+
+            "faculty": faculty[i],
+            "profession": profession[i],
+            "pay": pay[i],
+            "study_year": study_year[i]
+
+        })
+    }
+}
+
+fs.writeFileSync("profession.json", JSON.stringify(array))
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express();
 const fs = require('fs');
-const student = require("./student.json")
+const path = require('path')
+const pug = require('pug');
+// const student = require("./student.json")
 const port = 4000
+
+
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 app.get('/student-personal', (req, res) => {
 //    fs.writeFileSync('req.json', JSON.stringify(req.query))
-   res.send(student)
+   res.render('student-personal')
 })
 
 app.listen(port, () => {

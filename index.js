7af00c0343c2b4a447a689084ef94e41f9b5d412
sonaisrayel <israyelyan.sona@gmail.com>
2021-10-09 14:56:15 +0400

    Add changes

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express();
+require('dotenv').config()
 const fs = require('fs');
 const path = require('path')
 const pug = require('pug');
-// const student = require("./student.json")
-const port = 4000
-
+const port = process.env.port
+const student = require("./models/student-personal.json")
+const generateProfession = require('./models/generateProfession')
+const profession = require('./models/profession.json')
 
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.get('/student-personal', (req, res) => {
-//    fs.writeFileSync('req.json', JSON.stringify(req.query))
-   res.render('student-personal')
+  res.render('student-personal', { student: student })
+})
+
+app.get('/profession', (req, res) => {
+  res.render('profession', { profession: profession })
+})
+
+app.get('/generate', (req, res) => {
+  generateProfession
 })
 
 app.listen(port, () => {

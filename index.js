afe80a4de2e8d3a218ac327791df9cc062882b9c
lianaalaverdyan <liana.liana.alaverdyan55@gmail.com>
2021-11-02 20:12:12 +0400

    Add profession get one id in view

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ require('dotenv').config()
 const path = require('path')
 const port = process.env.port
 const bodyParser = require('body-parser')
-const generateProfession = require('./models/generateProfession')
+onst generateProfession = require('./models/generateProfession')
 const { getProfessions } = require("./controllers/profession-controller")
 const { getStudents, getStudent } = require("./controllers/student-controller")
-const { getFacultys, getFaculty } = require("./controllers/faculty-controller");
+const { getFacultys, getFaculty } = require("./controllers/faculty-controller")
+const { getProfessions, getProfession } = require("./controllers/profession-controller");
 
 app.use(bodyParser.json())
 
@@ -21,6 +22,7 @@ app.get('/facultys', getFacultys)
 app.get('/facultys/:id', getFaculty)
 
 app.get('/professions', getProfessions)
+app.get('/professions/:id', getProfession)
 
 app.get('/generate', (req, res) => {
   generateProfession

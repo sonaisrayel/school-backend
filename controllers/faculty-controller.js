484b2c5f877a9877f33adf3fc6db4037c4191e4c
Robert-Astanyan <robast43@gmail.com>
2021-11-30 12:09:32 +0400

    I changed some of the files

--- controllers/faculty-controller.js
+++ controllers/faculty-controller.js
@@ -1,4 +1,6 @@
-const faculty = require("../models/profession.json");
+const faculty = require("../models/faculty.json");
+const fs = require('fs');
+const Profession = require("../models/faculty-model.js")
 
 async function getFaculties(req, res) {
     res.render('faculty/faculty-list',{ faculty:faculty });
@@ -16,8 +18,15 @@ async function createFacultyView(req, res){
     res.render('faculty/faculty-create')
 }
 
+async function createFaculty (req, res){
+    const {profession,faculty,pay,study_year } = req.body
+    let proff = await Profession.create({profession,faculty,pay,study_year })
+    res.render('profession/profession-list', { profession: proff })
+}
+
 module.exports = {
     getFaculties,
     getFaculty,
-    createFacultyView
+    createFacultyView,
+    createFaculty,
 }

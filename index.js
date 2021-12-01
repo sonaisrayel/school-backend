5f5ffc213c9343d02b715a88d5cd9fcff93b115f
lianaalaverdyan <liana.liana.alaverdyan55@gmail.com>
2021-12-01 22:53:50 +0400

    Add profession.pug

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const mongoDb = require('./libs/db')
 mongoDb.connect();
 const {  getStudents,createStudent,createStudentView,getStudent } = require("./controllers/student-controller");
 
-const { getProfessions, getProfession,createProfessionView,deleteProfession, createProfession } = require("./controllers/profession-controller");
+const {  getProfessions, getProfession,createProfessionView,deleteProfession, createProfession,  editProfessionView, editProfession  } = require("./controllers/profession-controller");
+
+//const { getProfessions, getProfession,createProfessionView,deleteProfession, createProfession } = require("./controllers/profession-controller");
 //const { getStudents, getStudent,createStudent,createStudentView,editStudent, editStudentView } = require("./controllers/student-controller");
 const { getFaculty, getFaculties, createFacultyView} = require("./controllers/faculty-controller");
 const { getTeachers, getTeacher, createTeacher, createTeacherView } = require("./controllers/teacher-controller.js");
@@ -44,11 +46,15 @@ app.get('/faculties', getFaculties);
 app.get('/faculties/:id', getFaculty);
 app.get('/faculty/create', createFacultyView);
 
+//profession
 app.get('/professions', getProfessions);
 app.get('/professions/:id', getProfession);
 app.get('/professions/create', createProfessionView);
 app.get('/professions/delete/:id', deleteProfession);
 app.post('/professions',createProfession);
+app.get('/professions/edit/:id', editProfessionView);
+app.post('/professions/edit',editProfession);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
